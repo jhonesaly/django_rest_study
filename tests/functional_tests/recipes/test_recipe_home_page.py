ccaa254a04b36aea4d1ce6cba0c5df37b53a1bcd
Jhones <jhonesaly@gmail.com>
2023-04-03 18:03:03 -0300

    fix: remove emoji

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -12,7 +12,7 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
     def test_recipe_home_page_without_recipes_not_found_message(self):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
-        self.assertIn('No recipes found here 🥲', body.text)
+        self.assertIn('No recipes found here', body.text)
 
     @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_search_input_can_find_correct_recipes(self):
