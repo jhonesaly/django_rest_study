777fd88752a8fc7be5567586151755245de1ccd2
Jhones <jhonesaly@gmail.com>
2023-04-04 07:39:34 -0300

    feat: view for detail API

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -9,4 +10,14 @@ from ..serializers import RecipeSerializer
 def recipe_api_list(request):
     recipes = Recipe.objects.get_published()[:10]
     serializer = RecipeSerializer(instance=recipes, many=True)
+    return Response(serializer.data)
+
+
+@api_view()
+def recipe_api_detail(request, pk):
+    recipe = get_object_or_404(
+        Recipe.objects.get_published(),
+        pk=pk
+    )
+    serializer = RecipeSerializer(instance=recipe, many=False)
     return Response(serializer.data)
